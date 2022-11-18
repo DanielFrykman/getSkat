66f9af3564473c8f7a9831ce9eeef2f6fc3d255a
Daniel Frykman <danielfrykman@Daniels-MacBook-Pro.local>
2022-11-18 17:05:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+/**
+ * Rulls the lates rate for tax free driving allowance from skat.dk.
+ * Rulls the rate for driving in own car after 20.000km/year.
+ * Returns the rate as a number with a , as decimal point.
+ * 
+ * Henter satsen for skattefri kørselsgodkørelse fra skat.dk.
+ * Henter satsen for kørsel i egen bil over 20.000km/år.
+ * Returnerer satsen som et nummer med et , som decimalpuunkt.
+ */
+function getSkatdk() {
+  var url = "https://skat.dk/data.aspx?oid=2234870"
+  var websiteContent = UrlFetchApp.fetch(url).getContentText();
+  Logger.log(websiteContent);
+
+  //finds "Egen cykel, knallert eller EU-knallert" i the html and includes the 32 characters before that. 
+  //finder "Egen cykel, knallert eller EU-knallert" i html koden og går 32 tegn/mellemrum tilbage.
+  var RegExp1 = new RegExp(/(.{32})(Egen cykel, knallert eller EU-knallert)/gm); 
+  
+  // extracts the numbers
+  //tager tallet ud
+  var RegExp2 = new RegExp(/(\d+,\d+)/gm); 
+  
+  var matchText = websiteContent.match(RegExp1);
+  var matchText2 = matchText[0].match(RegExp2);
+
+  //should be commented out if decimal point should be . insted of ,
+  //skal udkommentares ud hvis decimal punktet skal være . istedet for ,
+  matchText2 = Number(matchText2[0].replace(/,/g, '.'))
+  
+  return matchText2;
+}
+
+
+
